247eb59752f675281d20b4e5759a7130f04f8af2
mas ade <ade_ari_w0@yahoo.com>
2012-10-20 19:29:43 +0700

    edfef

--- konek.php
+++ konek.php
@@ -1,4 +1,4 @@
 <?
-$link = mysql_connect('mysql.idhostinger.com', 'u415717661_webno', 'webnonoy');
-mysql_select_db("u415717661_webnonoy", $link);
+$link = mysql_connect('localhost', 'ade', '123');
+mysql_select_db("test", $link);
 ?>
